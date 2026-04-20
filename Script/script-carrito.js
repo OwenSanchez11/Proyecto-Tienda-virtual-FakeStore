b116c2c168c9da9788c4d34071fa724a75b51067
Owen Sanchez <owensanchez1104@gmail.com>
2026-04-20 15:40:31 -0500

    commit - plantilla carrito de compras

---
+++ Script/script-carrito.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', (e) => {
+    const nav = document.getElementById('nav');
+
+    if(e.target.closest('#abrir')) {
+        nav.classList.add('visible')
+    }
+
+    if(e.target.closest('#cerrar')) {
+        nav.classList.remove('visible');
+    }
+})
